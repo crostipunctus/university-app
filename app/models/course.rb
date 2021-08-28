4f1f3bc10780b1a5ade02408067b275b650acff5
crostipunctus <63543350+crostipunctus@users.noreply.github.com>
2021-08-28 21:55:12 +0530

    many to many

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,7 @@ class Course < ApplicationRecord
   validates :short_name, presence: true, length: { minimum: 4, maximum: 8 }
   validates :name, presence: true, length: { minimum: 4, maximum: 50 }
   validates :description, presence: true, length: { minimum: 10, maximum: 200}
+  has_many :student_courses 
+  has_many :students, through: :student_courses
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_26_065030) do
+ActiveRecord::Schema.define(version: 2021_08_28_134818) do
 
   create_table "courses", force: :cascade do |t|
     t.string "short_name"
     t.string "name"
     t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "student_courses", force: :cascade do |t|
+    t.integer "student_id"
+    t.integer "course_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
